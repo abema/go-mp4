1746392475a11eb7896a3f82e5ed1342d9058908
aler9 <46489434+aler9@users.noreply.github.com>
2022-10-21 15:26:07 +0200

    Improve compatibility of esds boxes
    Firefox and Chrome aren't currently able to read varints with a size
    different than 4. ISO 14496-1 isn't clear on the matter, although it
    limits the maximum varint size to 4.
    
    This patch forces varints to have a size of 4, allowing a broader
    compatibility of esds boxes. Sizes greater than 4 are not supported
    anymore (but this is dictated by the specification).

--- box_types_test.go
+++ box_types_test.go
@@ -381,7 +381,7 @@ func TestBoxTypes(t *testing.T) {
 				Descriptors: []Descriptor{
 					{
 						Tag:  ESDescrTag,
-						Size: 0x12345678,
+						Size: 0x1234567,
 						ESDescriptor: &ESDescriptor{
 							ESID:                 0x1234,
 							StreamDependenceFlag: true,
@@ -394,7 +394,7 @@ func TestBoxTypes(t *testing.T) {
 					},
 					{
 						Tag:  ESDescrTag,
-						Size: 0x12345678,
+						Size: 0x1234567,
 						ESDescriptor: &ESDescriptor{
 							ESID:                 0x1234,
 							StreamDependenceFlag: false,
@@ -407,7 +407,7 @@ func TestBoxTypes(t *testing.T) {
 					},
 					{
 						Tag:  DecoderConfigDescrTag,
-						Size: 0x12345678,
+						Size: 0x1234567,
 						DecoderConfigDescriptor: &DecoderConfigDescriptor{
 							ObjectTypeIndication: 0x12,
 							StreamType:           0x15,
@@ -435,40 +435,40 @@ func TestBoxTypes(t *testing.T) {
 				0,                // version
 				0x00, 0x00, 0x00, // flags
 				//
-				0x03,                         // tag
-				0x81, 0x91, 0xD1, 0xAC, 0x78, // size (varint)
+				0x03,                   // tag
+				0x89, 0x8d, 0x8a, 0x67, // size (varint)
 				0x12, 0x34, // esid
 				0xa3,       // flags & streamPriority
 				0x23, 0x45, // dependsOnESID
 				0x34, 0x56, // ocresid
 				//
-				0x03,                         // tag
-				0x81, 0x91, 0xD1, 0xAC, 0x78, // size (varint)
+				0x03,                   // tag
+				0x89, 0x8d, 0x8a, 0x67, // size (varint)
 				0x12, 0x34, // esid
 				0x43,                                                  // flags & streamPriority
 				11,                                                    // urlLength
 				'h', 't', 't', 'p', ':', '/', '/', 'h', 'o', 'g', 'e', // urlString
 				//
-				0x04,                         // tag
-				0x81, 0x91, 0xD1, 0xAC, 0x78, // size (varint)
+				0x04,                   // tag
+				0x89, 0x8d, 0x8a, 0x67, // size (varint)
 				0x12,             // objectTypeIndication
 				0x56,             // streamType & upStream & reserved
 				0x12, 0x34, 0x56, // bufferSizeDB
 				0x12, 0x34, 0x56, 0x78, // maxBitrate
 				0x23, 0x45, 0x67, 0x89, // avgBitrate
 				//
-				0x05,             // tag
-				0x03,             // size (varint)
+				0x05,                   // tag
+				0x80, 0x80, 0x80, 0x03, // size (varint)
 				0x11, 0x22, 0x33, // data
 				//
-				0x06,                         // tag
-				0x05,                         // size (varint)
+				0x06,                   // tag
+				0x80, 0x80, 0x80, 0x05, // size (varint)
 				0x11, 0x22, 0x33, 0x44, 0x55, // data
 			},
 			str: `Version=0 Flags=0x000000 Descriptors=[` +
-				`{Tag=ESDescr Size=305419896 ESID=4660 StreamDependenceFlag=true UrlFlag=false OcrStreamFlag=true StreamPriority=3 DependsOnESID=9029 OCRESID=13398}, ` +
-				`{Tag=ESDescr Size=305419896 ESID=4660 StreamDependenceFlag=false UrlFlag=true OcrStreamFlag=false StreamPriority=3 URLLength=0xb URLString="http://hoge"}, ` +
-				`{Tag=DecoderConfigDescr Size=305419896 ObjectTypeIndication=0x12 StreamType=21 UpStream=true Reserved=false BufferSizeDB=1193046 MaxBitrate=305419896 AvgBitrate=591751049}, ` +
+				`{Tag=ESDescr Size=19088743 ESID=4660 StreamDependenceFlag=true UrlFlag=false OcrStreamFlag=true StreamPriority=3 DependsOnESID=9029 OCRESID=13398}, ` +
+				`{Tag=ESDescr Size=19088743 ESID=4660 StreamDependenceFlag=false UrlFlag=true OcrStreamFlag=false StreamPriority=3 URLLength=0xb URLString="http://hoge"}, ` +
+				`{Tag=DecoderConfigDescr Size=19088743 ObjectTypeIndication=0x12 StreamType=21 UpStream=true Reserved=false BufferSizeDB=1193046 MaxBitrate=305419896 AvgBitrate=591751049}, ` +
 				"{Tag=DecSpecificInfo Size=3 Data=[0x11, 0x22, 0x33]}, " +
 				"{Tag=SLConfigDescr Size=5 Data=[0x11, 0x22, 0x33, 0x44, 0x55]}]",
 		},
